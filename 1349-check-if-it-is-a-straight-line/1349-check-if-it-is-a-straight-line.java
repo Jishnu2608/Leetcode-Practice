106bfad0892638061a149f3e937a6991fb801e21
Jishnudeep Borah <85721902+Jishnu2608@users.noreply.github.com>
2023-10-15 02:22:45 +0530

    Time: 0 ms (100%), Space: 43.2 MB (20.15%) - LeetHub

---
+++ 1349-check-if-it-is-a-straight-line/1349-check-if-it-is-a-straight-line.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+        if(coordinates.length < 3){
+            return true;
+        }
+
+        double initialSlope = calculate(coordinates[0], coordinates[1]);
+        for(int i=2;i<coordinates.length;i++){
+            double current = calculate(coordinates[i-1], coordinates[i]);
+            if(current != initialSlope){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private double calculate(int[] x, int[] y){
+
+        if(y[0] - x[0] == 0){
+            return Double.POSITIVE_INFINITY;
+        }else{
+            return (double) (y[1] - x[1]) / (y[0] - x[0]);
+        }       
+    }
+}
