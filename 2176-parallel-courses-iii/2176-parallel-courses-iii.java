db80c48cfe610b7e567c1cbe9aa74709c24e35bc
Jishnudeep Borah <85721902+Jishnu2608@users.noreply.github.com>
2023-10-18 18:29:31 +0530

    Time: 20 ms (75.56%), Space: 78.3 MB (7.56%) - LeetHub

--- 2176-parallel-courses-iii/2176-parallel-courses-iii.java
+++ 2176-parallel-courses-iii/2176-parallel-courses-iii.java
@@ -1,56 +1,37 @@
-class Solution {
-    List<List<Integer>> adjacencyList;  
-    List<Integer> inDegrees; 
-
-    // Kahn's Algorithm
-    public int calculateMinimumTime(int n, int[] time) {
-        int[] waitTime = new int[n + 1];  
-        Queue<Integer> q = new LinkedList<>(); 
-        int maxWaitTime = 0;  
-
-        for (int i = 1; i <= n; i++) {
-            if (inDegrees.get(i) == 0) {
-                q.offer(i);
-                waitTime[i] = time[i - 1];
-                maxWaitTime = Math.max(maxWaitTime, waitTime[i]);
-            }
+public class Solution {
+    public int minimumTime(int n, int[][] relations, int[] time) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
         }
 
-       
-        while (!q.isEmpty()) {
-            int currentCourse = q.poll();
-
-            
-            for (int childCourse : adjacencyList.get(currentCourse)) {
-                inDegrees.set(childCourse, inDegrees.get(childCourse) - 1);
-                waitTime[childCourse] = Math.max(time[childCourse - 1] + waitTime[currentCourse], waitTime[childCourse]);
-                maxWaitTime = Math.max(maxWaitTime, waitTime[childCourse]);
-
-               
-                if (inDegrees.get(childCourse) == 0)
-                    q.offer(childCourse);
-            }
+        for (int[] relation : relations) {
+            int prev = relation[0] - 1;
+            int next = relation[1] - 1;
+            graph.get(next).add(prev);
         }
 
-        return maxWaitTime;
+        int[] memo = new int[n];
+        int overallMinTime = 0;
+
+        for (int i = 0; i < n; i++) {
+            overallMinTime = Math.max(overallMinTime, calculateTime(i, graph, time, memo));
+        }
+
+        return overallMinTime;
     }
 
-    public int minimumTime(int n, int[][] relations, int[] time) {
-        adjacencyList = new ArrayList<>(n + 1);
-        inDegrees = new ArrayList<>(n + 1);
-
-        for (int i = 0; i <= n; i++) {
-            adjacencyList.add(new ArrayList<>());
-            inDegrees.add(0);
+    private int calculateTime(int course, List<List<Integer> > graph, int[] time, int[] memo) {
+        if (memo[course] != 0) {
+            return memo[course];
         }
 
-     
-        for (int i = 0; i < relations.length; i++) {
-            int[] prerequisitePair = relations[i];
-            adjacencyList.get(prerequisitePair[0]).add(prerequisitePair[1]);
-            inDegrees.set(prerequisitePair[1], inDegrees.get(prerequisitePair[1]) + 1);
+        int maxPrerequisiteTime = 0;
+        for (int prereq : graph.get(course)) {
+            maxPrerequisiteTime = Math.max(maxPrerequisiteTime, calculateTime(prereq, graph, time, memo));
         }
 
-        return calculateMinimumTime(n, time);
+        memo[course] = maxPrerequisiteTime + time[course];
+        return memo[course];
     }
 }
